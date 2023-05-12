ad2f2c6a3de4096aefe072664bd70f3e418b609f
rrizun <rrizun@rrizuns-MacBook-Air.local>
2023-05-12 08:03:55 -0700

    setup.py: systemd

--- setup.py
+++ setup.py
@@ -1,2 +1,2 @@
 import setuptools
-setuptools.setup(name='inf-agent', scripts=['inf-agent.py'])
+setuptools.setup(name='inf-agent', scripts=['inf-agent.py'], data_files=[('/etc/systemd/system', ['etc/systemd/system/cirrascale-inf-agent.service'])])
