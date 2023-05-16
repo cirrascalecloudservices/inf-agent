400f847069210e9261eb2a18583979c9606e234c
rrizun <rrizun@gmail.com>
2023-05-16 12:34:23 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,2 @@
 import setuptools
-setuptools.setup(name='inf-agent', scripts=['inf-agent.py'], data_files=[('/etc/systemd/system', ['etc/systemd/system/cirrascale-inf-agent.service'])])
+setuptools.setup(name='inf-agent', scripts=['inf-agent.py'], data_files=[('etc/systemd/system', ['etc/systemd/system/cirrascale-inf-agent.service'])])
